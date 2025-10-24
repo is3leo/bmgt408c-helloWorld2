88a7c48178122ab5e863e69cc5c810dafbd6cad1
Isabelle Leo <ileo@terpmail.umd.edu>
2025-10-24 19:16:36 -0400

    favorite course and contact pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -22,6 +22,28 @@ def about_css():
 @app.route('/greeting')
 def greeting():
     return render_template('greeting.html')
+
+@app.route('/favorite-course')
+def favorite_course():
+    subject = request.args.get('subject', '')
+    course_number = request.args.get('course_number', '')
+    return render_template('favorite-course.html', subject=subject, course_number=course_number)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')  # Additional field
+        return render_template('contact.html',
+                             submitted=True,
+                             first_name=first_name,
+                             last_name=last_name,
+                             email=email,
+                             phone=phone)
+    else:
+        return render_template('contact.html', submitted=False)
 
 if __name__ == '__main__':
     app.run()
